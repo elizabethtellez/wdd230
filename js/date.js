1c05ec0a247a462d365844bd516b89d440472407
elizabethtellez <tel21004@byui.edu>
2022-05-04 17:17:47 -0700

    current year correction

--- js/date.js
+++ js/date.js
@@ -1,2 +1,4 @@
 const lastmod = document.querySelector("#lastmod");
 lastmod.innerHTML = `<em> Last Modification: ${document.lastModified}</em`;
+
+document.getElementById("year").innerHTML = new Date(). getFullYear();
